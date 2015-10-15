753810166e6e770d73389437737b7f96c07967dd
Brice Sanchez <brice@bisscomm.com>
2015-10-15 09:48:23 -0400

    Fix migration rollback and add missing category rebuild

--- db/migrate/6_add_nested_set_fields_to_categories.rb
+++ db/migrate/6_add_nested_set_fields_to_categories.rb
@@ -1,10 +1,22 @@
 class AddNestedSetFieldsToCategories < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :refinery_teams_categories, :parent_id, :integer
     add_column :refinery_teams_categories, :lft, :integer
     add_column :refinery_teams_categories, :rgt, :integer
     add_column :refinery_teams_categories, :depth, :integer
 
     Refinery::Teams::Category.add_translation_fields! slug: :string
+
+    # This is necessary to update :lft and :rgt columns
+    ::Refinery::Teams::Category.rebuild!
+  end
+
+  def self.down
+    remove_column :refinery_teams_categories, :parent_id
+    remove_column :refinery_teams_categories, :lft
+    remove_column :refinery_teams_categories, :rgt
+    remove_column :refinery_teams_categories, :depth
+
+    remove_column :refinery_teams_category_translations, :slug
   end
 end
